231ab50a70d5623cf283c81e464e62efb5e3fbb1
Evan Butler <evanb2@mac.com>
2015-03-20 21:55:06 -0700

    edit styles.css

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3aadff9860f0ffb5efe2704b99a6f6ab::getLoader();
+return ComposerAutoloaderInit985ecb15cf5a0e24b8cf5e772e988067::getLoader();
